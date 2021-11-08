d88de305ab4af385fc4080d93d2accb9366bf113
Furkan Yabancı <yabancifurkan01@gmail.com>
2021-11-08 20:24:56 +0300

    Prime Number

---
+++ src/primeNumber.js
@@ -0,0 +1,31 @@
+
+let isPrime = true
+function prime(...numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] < 2) {
+            console.log( numbers[i]  +" --> 2 den büyük sayı giriniz")
+        }
+        else if (numbers[i] == 2) {
+            console.log(numbers[i] + " asal sayıdır")
+        }
+        else {
+            for (let j = 2; j < numbers[i]; j++) {
+                if (numbers[i] % j == 0) {
+                    isPrime = false
+                    break
+                }   
+                else{
+                    isPrime = true
+                }
+            }
+                if (isPrime == true) {
+                    console.log(numbers[i] + " asal sayıdır.")
+                }
+                else {
+                    console.log(numbers[i] + " asal sayı değildir.")
+                }
+        }
+    }
+}
+
+prime(-23, 23, 37, 40,99,101,103,107,241,1)
